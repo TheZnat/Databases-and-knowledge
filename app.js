e5c157292bd0707353ecbc67ddc32f52316411e1
Alexey Subbotin <alesu1543@gmail.com>
2024-06-19 19:51:33 +0300

    fix cols in row

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// католог с модулем для синхр. работы с MySQL, который должен быть усталовлен командой: sync-mysql
-const dir_nms = 'C:\\nodejs\\node_modules\\sync-mysql';
-
-// работа с базой данных.
-const Mysql = require(dir_nms)
+// работа с базой данных (через модуль для синхр. работы с MySQL, который должен быть усталовлен командой: sync-mysql)
+const Mysql = require('C:\\nodejs\\node_modules\\sync-mysql');
 const connection = new Mysql({
-    host:'localhost', 
-    user:'root', 
-    password:'', 
-    database:'test'
-})
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+});
 
 // обработка параметров из формы.
 var qs = require('querystring');
-function reqPost (request, response) {
+function reqPost(request, response) {
     if (request.method == 'POST') {
         var body = '';
 
@@ -24,22 +21,29 @@ function reqPost (request, response) {
 			var post = qs.parse(body);
 			var sInsert = "INSERT INTO myarttable (text, description, keywords) VALUES (\""+post['col1']+"\",\""+post['col2']+"\",\""+post['col3']+"\")";
 			var results = connection.query(sInsert);
-            console.log('Done. Hint: '+sInsert);
+            console.log('Done. Hint: ' + sInsert);
         });
     }
 }
 
 // выгрузка массива данных.
 function ViewSelect(res) {
-	var results = connection.query('SHOW COLUMNS FROM myarttable');
-	res.write('<tr>');
-	for(let i=0; i < results.length; i++)
-		res.write('<td>'+results[i].Field+'</td>');
-	res.write('</tr>');
+    const query = 'SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC';
+    const results = connection.query(query);
 
-	var results = connection.query('SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC');
-	for(let i=0; i < results.length; i++)
-		res.write('<tr><td>'+String(results[i].id)+'</td><td>'+results[i].text+'</td><td>'+results[i].description+'</td><td>'+results[i].keywords+'</td></tr>');
+    res.write('<tr>');
+    for (let key in results[0]) {
+        res.write('<td>' + key + '</td>');
+    }
+    res.write('</tr>');
+
+    for (let row of results) {
+        res.write('<tr>');
+        for (let key in row) {
+            res.write('<td>' + row[key] + '</td>');
+        }
+        res.write('</tr>');
+    }
 }
 function ViewVer(res) {
 	var results = connection.query('SELECT VERSION() AS ver');
@@ -53,9 +57,8 @@ const server = http.createServer((req, res) => {
 	console.log('Loading...');
 	
 	res.statusCode = 200;
-//	res.setHeader('Content-Type', 'text/plain');
 
-	// чтение шаблока в каталоге со скриптом.
+	// чтение шаблона в каталоге со скриптом.
 	var fs = require('fs');
 	var array = fs.readFileSync(__dirname+'\\select.html').toString().split("\n");
 	console.log(__dirname+'\\select.html');
@@ -73,5 +76,5 @@ const server = http.createServer((req, res) => {
 const hostname = '127.0.0.1';
 const port = 3000;
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
