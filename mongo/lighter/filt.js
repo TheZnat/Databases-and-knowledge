b0c98190ea6aadd1a335bd3d38bba48a4c303cb7
Alexey Subbotin <alesu1543@gmail.com>
2024-06-30 15:20:00 +0300

    add lt

---
+++ mongo/lighter/filt.js
@@ -0,0 +1,20 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+
+async function run() {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("usersdb");
+        const collection = db.collection("users");
+        const results = await collection.find({age: 40}).toArray();
+        console.log(results);
+
+    }catch(err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    }
+}
+run().catch(console.error);
